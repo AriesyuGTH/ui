24869c9ba10a9f5271da9d28bffa3da8687a95ad
AriesGTH <rockyr.soul@gmail.com>
2022-06-05 21:58:07 +0800

    adding git ignore

---
+++ HelloNative.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class HelloNative {
+    static {
+        System.loadLibrary("HelloNative");
+    }
+
+    public static native void sayHello();
+
+    @SuppressWarnings("static-access")
+    public static void main(String[] args) {
+        new HelloNative().sayHello();
+    }
+}
