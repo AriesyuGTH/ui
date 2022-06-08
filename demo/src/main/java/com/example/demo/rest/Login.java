f418ba5bd536191e3f4649607036407381b6fe84
AriesGTH <rockyr.soul@gmail.com>
2022-06-08 16:33:01 +0800

    app、rest、service、config分離

---
+++ demo/src/main/java/com/example/demo/rest/Login.java
@@ -0,0 +1,38 @@
+package com.example.demo.rest;
+
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Login {
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        try {
+
+            return Collections.singletonMap("name", principal.getAttribute("name"));
+        } catch (Exception e) {
+            logger.error("[{}] /user 路徑發生錯誤了，原因是：", ExceptionUtils.getStackTrace(e));
+            return Collections.emptyMap();
+        }
+    }
+
+    @GetMapping("/error")
+    public String error(HttpServletRequest request) {
+        try {
+            String message = (String) request.getSession().getAttribute("error.message");
+            request.getSession().removeAttribute("error.message");
+            return message;
+        } catch (Exception e) {
+            logger.error("[{}] /error 路徑發生錯誤了，原因是：", ExceptionUtils.getStackTrace(e));
+            return null;
+        }
+    }
+}
